900f7b114a403287174a59720430dc2f70fc53cf
nettlesoup <sanettleingham@hotmail.co.uk>
2020-10-10 15:12:36 +0100

    Basic express typescript integration

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,20 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithBody extends Request {
+	body: { [key: string]: string | undefined };
+}
 
 const router = Router();
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+	if (req.session && req.session.loggedIn) {
+		next();
+		return;
+	}
+
+	res.status(403);
+	res.send('Not permitted');
+}
 
 router.get('/login', (req: Request, res: Response) => {
 	res.send(`
@@ -18,10 +32,48 @@ router.get('/login', (req: Request, res: Response) => {
   `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
 	const { email, password } = req.body;
 
-	res.send(email.toUpperCase());
+	if (
+		email &&
+		password &&
+		email === 'test@test.com' &&
+		password === 'password'
+	) {
+		req.session = { loggedIn: true };
+		res.redirect('/');
+		// res.send(email.toUpperCase());
+	} else {
+		res.send('Invalid login');
+	}
+});
+
+router.get('/', (req: Request, res: Response) => {
+	if (req.session && req.session.loggedIn) {
+		res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+	} else {
+		res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+	}
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+	req.session = null;
+	res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+	res.send('Welcome logged in user');
 });
 
 export { router };
