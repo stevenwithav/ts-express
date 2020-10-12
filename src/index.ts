dbe012fa149ccdc5974f70634a06b2e68c59d3a2
nettlesoup <sanettleingham@hotmail.co.uk>
2020-10-12 12:43:14 +0100

    Refactor with typescript decorators

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+
+import { AppRouter } from './appRouter';
+
+import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['qwerty'] }));
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
